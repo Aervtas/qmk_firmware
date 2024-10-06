32520578e75c64a8a49c6a8f80909baa86e510b4
Kevin <klin0916@gmail.com>
2024-10-05 23:55:31 -0400

    periodic update

--- keyboards/keebio/chocopad/keymaps/Kevin/keymap.c
+++ keyboards/keebio/chocopad/keymaps/Kevin/keymap.c
@@ -10,7 +10,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_ortho_4x4(
-    KC_P7,    KC_P8,    KC_P9,   KC_END,
+    KC_P7,    KC_P8,    KC_P9,   KC_NUM,
     KC_P4,    KC_P5,    KC_P6,   KC_RGHT,
     KC_P1,    KC_P2,    KC_P3,   KC_MPRV,
     MO(_FN2), KC_P0,    KC_DOT,  KC_P0
